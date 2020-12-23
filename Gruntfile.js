5b1f4c5043206813c7880c419e82faaaed2114f6
Richard French <68925499+rfrench2@users.noreply.github.com>
2020-12-23 11:17:20 -0500

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -196,7 +196,6 @@ module.exports = function(grunt) {
                 }
             },
             options: {
-                implementation: sass,
                 includePaths: ["theme/boost/scss/", "theme/classic/scss/"]
             }
         },
@@ -365,8 +364,6 @@ module.exports = function(grunt) {
     tasks.gherkinlint = function() {
         const done = this.async();
         const options = grunt.config('gherkinlint.options');
-        
-        const sass = require('node-sass');
 
         // Grab the gherkin-lint linter and required scaffolding.
         const linter = require('gherkin-lint/src/linter.js');
